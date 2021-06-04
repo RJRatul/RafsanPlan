9ffb0b8b66d2c8446fd5fe440a320da1c64a4007
RJRatul <rafsan.ratul.mians@gmail.com>
2021-06-04 23:39:00 +0600

    NavBar Designing

--- rafsanplan/src/App.js
+++ rafsanplan/src/App.js
@@ -1,13 +1,16 @@
-import React from "react"
+import React, {Component} from "react"
 import {BrowserRouter} from "react-router-dom"
-function App() {
+import Navbar from './components/layout/Navbar'
+class App extends Component {
+  render () {
   return (
     <BrowserRouter>
     <div className="App">
-      <h1>Rafsan's Plan</h1>
+      <Navbar/>
     </div>
     </BrowserRouter>
   );
+}
 }
 
 export default App;
