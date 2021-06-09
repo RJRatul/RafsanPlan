533ed37c3b24bf327fe5c17ba3febc1b5b34a64f
RJRatul <rafsan.ratul.mians@gmail.com>
2021-06-10 05:18:33 +0600

    Redux Working Check

--- rafsanplan/src/store/reducers/projectReducer.js
+++ rafsanplan/src/store/reducers/projectReducer.js
@@ -1,9 +1,9 @@
 const initState = {
-    // projects:[
-    //     {id: '1', title:'Help me bitch', content:'blah blah blah'},
-    //     {id: '2', title:'No bitch', content:'blah blah blah'},
-    //     {id: '3', title:'Stop bitch', content:'blah blah blah'}
-    // ]
+    projects:[
+        {id: '1', title:'Help me bitch', content:'blah blah blah'},
+        {id: '2', title:'No bitch', content:'blah blah blah'},
+        {id: '3', title:'Stop bitch', content:'blah blah blah'}
+    ]
 }
 
 const projectReducer = (state = initState, action) => {
